ce58e8de4645c5a0c97bbfcd9bd8f281e51e1520
khadkakrijal <khadkakrijal36@gmail.com>
2026-03-02 17:27:14 +0930

    build issue fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import MasterLayout from "@/Components/Masterlayout/Master";
-
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <MasterLayout> {children}</MasterLayout>
+        <Suspense fallback={null}>
+          <MasterLayout> {children}</MasterLayout>
+        </Suspense>
       </body>
     </html>
   );
